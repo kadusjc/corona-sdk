a2fbc06e67deaba2e72416e09617bb86d15a4cc2
Carlos Eduardo Ferreiro Corrêa <kadusjc@yahoo.com.br>
2015-07-31 10:37:56 -0300

    Working with Strings

---
+++ Working-With-Strings/main.lua
@@ -0,0 +1,17 @@
+print("This is a string!") -- This is a string!
+print("15" + 1) -- Returns the value 16
+
+myVar = 28
+print(myVar) -- Returns 28
+
+myVar = "twenty-eight"
+print(myVar) -- Returns twenty-eight
+
+firstName, phone = "John Doe", "123-456-7890"
+secondName = "John Doe"
+
+print(firstName, phone) -- John Doe 123-456-7890
+print(firstName == phone) -- false
+print(firstName <= phone) -- false
+print(firstName == secondName) -- true
+print([["My 'name' is ]]..secondName..[["]] );
