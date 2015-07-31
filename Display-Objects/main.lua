3653b02832d32251c1c7db5955d7ca7216f9b070
Carlos Eduardo Ferreiro Corrêa <kadusjc@yahoo.com.br>
2015-07-31 13:18:01 -0300

    Create main.lua

---
+++ Display-Objects/main.lua
@@ -0,0 +1,9 @@
+local background = display.newImage( "images/Texaco.png",  true );
+local image01 = display.newImage( "images/check.png", 110, 30 );
+local image02 = display.newImage( "images/check.png" );
+image02.x = 160; image02.y = 220;
+image03 = display.newImage( "images/check.png" );
+image03.x = 160; image03.y = 320;
+image01.x = 160; image01.y = 420;
+
+background.alpha = 0.4;
